764395c0dd2b0898b3620a3ebc1ae1da8ad7289d
alfinhimawan <alfinhimawan99@gmail.com>
2026-04-30 18:45:05 +0700

    Divexplore-3D-Update

--- Backend/src/routes/authRoutes.js
+++ Backend/src/routes/authRoutes.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
-const { authenticate } = require("../middlewares/authenticate");
+const { authenticate, authorize } = require("../middlewares/authenticate");
 
 // POST /api/auth/register
 router.post("/register", authController.register);
