f4bb00129af29cbb318ee5b681747e845f8831e7
Marizta <adityadanuu@gmail.com>
2026-05-07 12:07:33 +0700

    menambahkan fitur catalog dari ecommers

--- Frontend/src/app/routes/AppRouter.tsx
+++ Frontend/src/app/routes/AppRouter.tsx
@@ -4,6 +4,7 @@ import Hotspot3DPage from '../../features/dashboard/Hotspot3DPage';
 import LoginPage from '../../features/auth/LoginPage';
 import ProductDetailPage from '../../features/dashboard/ProductDetailPage';
 import CartPage from '../../features/cart/CartPage';
+import CatalogPage from '../../features/catalog/CatalogPage';
 
 export default function AppRouter() {
   return (
@@ -13,6 +14,7 @@ export default function AppRouter() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/product" element={<ProductDetailPage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/catalog" element={<CatalogPage />} />
       </Routes>
     </BrowserRouter>
   );
