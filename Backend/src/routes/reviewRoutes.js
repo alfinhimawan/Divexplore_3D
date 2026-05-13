3d5fd3773cab9b58ec0ae127dec0ab1cd48faa3b
alfinhimawan <alfinhimawan99@gmail.com>
2026-05-13 18:31:33 +0700

    fix(backend): use wrapper functions in reviewRoutes to prevent undefined handler error

--- Backend/src/routes/reviewRoutes.js
+++ Backend/src/routes/reviewRoutes.js
@@ -5,9 +5,12 @@ const reviewController = require("../controllers/reviewController");
 const { authenticate } = require("../middlewares/authenticate");
 
 // Memberikan review (harus login)
-router.post("/:orderId", authenticate, reviewController.createReview);
+router.post("/:orderId", 
+  (req, res, next) => authenticate(req, res, next), 
+  (req, res, next) => reviewController.createReview(req, res, next)
+);
 
 // Mengambil review produk (publik)
-router.get("/product/:productId", reviewController.getProductReviews);
+router.get("/product/:productId", (req, res, next) => reviewController.getProductReviews(req, res, next));
 
 module.exports = router;
